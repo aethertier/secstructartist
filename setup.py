c9806d775790c24e5a471ca9d24d51234aa09b93
David Bickel <david.bickel@vub.be>
2023-09-13 07:12:05 +0200

    Fix: Improve wording in examples.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fhandle:
 
 setup(
     name="secstructartist",
-    version="1.0.8",
+    version="1.0.9",
     author="David Bickel",
     author_email="athertier@mailueberfall.de",
     description="Module to visualize protein secondary structure in matplotlib plots",
